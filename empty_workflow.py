21c2abee6201bbdc2951452572787a7ca49ca587
Yacouby <yacouby@iguazio.com>
2025-03-03 14:18:52 +0200

    update image to mlrun/mlrun

--- empty_workflow.py
+++ empty_workflow.py
@@ -3,5 +3,5 @@ from kfp import dsl
 
 @dsl.pipeline(name="test_pipeline", description="empty pipeline")
 def pipeline():
-    dsl.ContainerOp(name="test", image="python:3.9", command=["Testing !"])
+    dsl.ContainerOp(name="test", image="mlrun/mlrun", command=["Testing !"])
 
